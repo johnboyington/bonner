598b8658e0e8779d349f795e597afad37a730409
John Boyington <mjcb@ksu.edu>
2017-11-21 13:13:30 -0600

    plotter now compares each spheres function

--- nebp_response_functions/plotter.py
+++ nebp_response_functions/plotter.py
@@ -8,17 +8,22 @@ class Plotter(object):
     def __init__(self):
         Nice_Plots()
         self.get_data()
+        self.get_decker()
         self.set_coefficient()
         self.plot()
+        self.plot_comparison()
 
     def get_data(self):
         self.data = np.loadtxt('nebp_response_functions.txt').reshape(7, -1, 4)
+
+    def get_decker(self):
+        self.decker_data = np.loadtxt('decker_data.txt').reshape(7, -1, 4)
 
     def set_coefficient(self):
         self.c = 1.774E-2 * (1/4)
 
     def set_colors(self, i):
-        colors = ['blue', 'green', 'red', 'black', 'orange', 'violet', 'yellow']
+        colors = ['k', 'r', 'b', 'm', 'g', 'navy', 'purple']
         return colors[i]
 
     def set_labels(self, i):
@@ -26,7 +31,7 @@ class Plotter(object):
         return labels[i]
 
     def plot(self):
-        plt.figure(0)
+        plt.figure(68)
         for i, sphere in enumerate(self.data):
             sphere = sphere.T
             edges = np.insert(sphere[1], 0, 1E-11)
@@ -39,11 +44,43 @@ class Plotter(object):
             plt.errorbar(s.midpoints, s.values, s.error, linestyle='None', capsize=0, color=c)
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlim(1E-9, 20)
+        plt.xlim(1E-8, 20)
+        plt.ylim(1E-5, 2.5E-1)
         plt.xlabel('energy $MeV$')
         plt.ylabel('response $cm^{2}$')
         plt.legend(loc=3)
         plt.savefig('nebp_response_functions.png', dpi=250)
+        plt.close()
+
+    def plot_comparison(self):
+        for i, sphere in enumerate(self.data):
+            plt.figure(60 + i)
+            sphere = sphere.T
+            edges = np.insert(sphere[1], 0, 1E-11)
+            vals = sphere[2] * self.c
+            err = sphere[3] * vals
+            s = Spectrum(edges, vals, err)
+            c = self.set_colors(i)
+            l = self.set_labels(i)
+            plt.plot(s.stepu_x, s.stepu_y, color=c, label=l + ' Boyington')
+            plt.errorbar(s.midpoints, s.values, s.error, linestyle='None', capsize=0, color=c)
+            sphere = self.decker_data[i].T
+            edges = np.insert(sphere[1], 0, 1E-11)
+            vals = sphere[2] * self.c
+            err = sphere[3] * vals
+            s = Spectrum(edges, vals, err)
+            c = self.set_colors(i)
+            l = self.set_labels(i)
+            plt.plot(s.edges[1:], s.values, linestyle='--', color=c, label=l + ' Decker')
+            plt.errorbar(s.edges[1:], s.values, s.error, linestyle='None', capsize=0, color=c)
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.xlim(1E-8, 20)
+            plt.xlabel('energy $MeV$')
+            plt.ylabel('response $cm^{2}$')
+            plt.legend(loc=3)
+            plt.savefig('rf_comp{}.png'.format(l), dpi=250)
+            plt.close()
 
 
 if __name__ == '__main__':
