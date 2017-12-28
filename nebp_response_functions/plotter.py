8ed3de6c5a45abd0f2fb74e09a9d96db1f467621
John Boyington <mjcb@ksu.edu>
2017-12-28 15:30:07 -0600

    beatified rf plot

--- nebp_response_functions/plotter.py
+++ nebp_response_functions/plotter.py
@@ -1,18 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from nice_plots import Nice_Plots
 from spectrum import Spectrum
+from matplotlib import rc, rcParams
 
 
 class Plotter(object):
     def __init__(self):
-        Nice_Plots()
+        self.nice_plots()
         self.get_data()
         self.get_decker()
         self.set_coefficient()
         self.plot()
-        self.plot_comparison()
+        # self.plot_comparison()
         self.calc_peak_channels()
+
+    def nice_plots(self):
+        rc('font', **{'family': 'serif'})
+        rcParams['xtick.direction'] = 'out'
+        rcParams['ytick.direction'] = 'out'
+        rcParams['xtick.labelsize'] = 12
+        rcParams['ytick.labelsize'] = 12
+        rcParams['lines.linewidth'] = 1.85
+        rcParams['axes.labelsize'] = 15
+        rcParams.update({'figure.autolayout': True})
 
     def get_data(self):
         self.data = np.loadtxt('nebp_response_functions.txt').reshape(7, -1, 4)
@@ -24,33 +34,45 @@ class Plotter(object):
         self.c = 1.774E-2 * (1/4)
 
     def set_colors(self, i):
-        colors = ['k', 'r', 'b', 'm', 'g', 'navy', 'purple']
+        colors = ['k', 'purple', 'blue', 'green', 'orange', 'red', 'brown']
         return colors[i]
 
+    def set_linestyles(self, i):
+        lines = ['-', '-.', '--', ':', '-', '-.', '--']
+        return lines[i]
+
     def set_labels(self, i):
-        labels = ['bare', '2"', '3"', '5"', '8"', '10"', '12"']
+        labels = ['Bare', '2"', '3"', '5"', '8"', '10"', '12"']
         return labels[i]
 
     def plot(self):
-        plt.figure(68)
+        fig = plt.figure(68)
+        ax = fig.add_subplot(111)
         for i, sphere in enumerate(self.data):
             sphere = sphere.T
             edges = np.insert(sphere[1], 0, 1E-11)
             vals = sphere[2] * self.c
             err = sphere[3] * vals
             s = Spectrum(edges, vals, err)
-            c = self.set_colors(i)
-            l = self.set_labels(i)
-            plt.plot(s.stepu_x, s.stepu_y, color=c, label=l)
-            plt.errorbar(s.midpoints, s.values, s.error, linestyle='None', capsize=0, color=c)
-        plt.xscale('log')
-        plt.yscale('log')
-        plt.xlim(1E-8, 20)
-        plt.xlabel('energy $MeV$')
-        plt.ylabel('response $cm^{2}$')
-        plt.legend(loc=3)
-        plt.savefig('nebp_response_functions.png', dpi=250)
-        plt.close()
+            style = {'color': self.set_colors(i), 'linestyle': self.set_linestyles(i),
+                     'linewidth': 0.7, 'label': self.set_labels(i)}
+            ax.plot(s.stepu_x, s.stepu_y, **style)
+            style = {'color': self.set_colors(i), 'linestyle': 'None', 'linewidth': 0.7}
+            ax.errorbar(s.midpoints, s.values, s.error, **style)
+            if i == 0:
+                plus = 5E-3
+            else:
+                plus = 0
+            ax.text(1.2E-8, s.stepu_y[1] + plus, self.set_labels(i))
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xlim(1E-8, 20)
+        ax.set_xlabel('Energy $MeV$')
+        ax.set_ylabel('Response $cm^{2}$')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        fig.savefig('nebp_response_functions.png', dpi=300)
+        plt.close(fig)
 
     def plot_comparison(self):
         for i, sphere in enumerate(self.data):
